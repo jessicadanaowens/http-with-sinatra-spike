b9a5b622bdd2172a8aeea5755b75c585ebb549b7
Jessica Owens <jessicadanaowens@gmail.com>
2014-06-26 16:20:53 -0600

    sinatra first app

--- app.rb
+++ app.rb
@@ -1,17 +1,30 @@
 require "sinatra/base"
-require "sinatra/reloader"
 
 class MyApp < Sinatra::Application
-  register Sinatra::Reloader
 
   def initialize
     super
-    @items = []
+    @dogs = ["Henry", "Sam", "Jilly"]
+
   end
 
   get "/" do
-    "items length: #{@items.length}"
+    erb :root
   end
+
+  get "/dogs" do
+    filter = params[:filter]
+    selected_dogs = @dogs
+    if filter != nil
+      selected_dogs = @dogs.select do |dog| dog.downcase.include?(filter.downcase) end
+    end
+    erb :dogs, :locals =>{:selected_dogs => selected_dogs} # get me the dogs view file
+
+  end
+
+  get "/dogs/:bla" do
+    erb :dog
+    end
 
   run! if app_file == $0
 end
